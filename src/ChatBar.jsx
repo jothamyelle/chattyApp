a6c91d87dabfee3ff0491a961f77cadb24438cce
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-03 23:47:27 +0000

    Initial commit

---
+++ src/ChatBar.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ChatBar extends Component {
+  render() {
+    return (
+      <h1>Hello React :)</h1>
+    );
+  }
+}
+export default ChatBar;
---
+++ src/MessageList.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <h1>Hello React :)</h1>
+    );
+  }
+}
+export default MessageList;
