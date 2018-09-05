522fcd07a9bae204f74b62c39d90cd34aea3ae62
Jotham Yelle <jothamyelle@hotmail.com>
2018-09-05 04:29:54 +0000

    Users can now upload images to the chat

--- src/Message.jsx
+++ src/Message.jsx
@@ -12,6 +12,25 @@ class Message extends Component {
     } else {
       nameColor = {color: messageInfo.currentUserColor};
     }
+    let imgOrText = "";
+    if(messageInfo.content.toLowerCase().includes(".png") || messageInfo.content.toLowerCase().includes(".gif") || messageInfo.content.toLowerCase().includes(".jpg")) {
+      let imgAndTextArray = messageInfo.content.split(" ");
+      let imgURL = imgAndTextArray.map((item) => {
+        if(item.toLowerCase().includes(".png") || item.toLowerCase().includes(".gif") || item.toLowerCase().includes(".jpg")) {
+          return item;
+        }
+      });
+      imgURL = imgURL.join('');
+      let actualContent = imgAndTextArray.map((item) => {
+        if(!(item.toLowerCase().includes(".png") || item.toLowerCase().includes(".gif") || item.toLowerCase().includes(".jpg"))) {
+          return item + " ";
+        }
+      });
+      imgOrText = (<span className="message-content"><span>{actualContent}</span><br/><img src={imgURL} className="message-content img-content"/></span>);
+    } else {
+      imgOrText = (<span className="message-content">{messageInfo.content}</span>);
+    }
+
     let htmlToDisplay = "";
     if (messageInfo.type && messageInfo.type.includes("Notification")) {
       htmlToDisplay = 
@@ -22,7 +41,7 @@ class Message extends Component {
       htmlToDisplay = 
       <div id={messageInfo.id} className="message">
         <span className="message-username" style={nameColor}>{messageInfo.username}</span>
-        <span className="message-content">{messageInfo.content}</span>
+        {imgOrText}
       </div>;
     }
 
